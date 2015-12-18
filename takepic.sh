02f05ecbed255530d0ff372e76e9a2f3dd006704
lor21 <loroof@gmail.com>
2015-12-18 02:11:21 +0000

    Wait longer after unmounting

--- takepic.sh
+++ takepic.sh
@@ -1,9 +1,9 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
 DATE=`date +%s`
 
 gvfs-mount -s gphoto2
-sleep 5
+sleep 10
 gphoto2 --capture-image-and-download --keep --filename ${DATE}.jpg
 epiphany-browser ${DATE}.jpg
 
